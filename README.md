986de45ebde3eaee56b8a0c5eb505d3459aa554c
Hadrien Milano <hadrien.milano@gmail.com>
2018-08-25 12:25:08 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Docker-based TuringWars infrastructure
+
+**WIP: TODO next: load mysql creds securely**
+
+To start the server, run:
+```
+./start.sh
+```
+
+This pulls the latest release from github and runs it in docker.
+
+Use this command instead if you want to run the code from a local clone of the [turingwars/turingwars](https://github.com/turingwars/turingwars) repository.
+
+```
+./start.sh --source path/to/local/turingwars
+```
---
+++ start.sh
@@ -0,0 +1,40 @@
+#!/bin/sh -xe
+
+get_latest_release() {
+    curl https://api.github.com/repos/turingwars/turingwars/releases/latest | grep tag_name | cut -f4 '-d"'
+}
+
+load_from_github() {
+    TURINGWARS_VERSION=$(get_latest_release)
+    echo "Using version $TURINGWARS_VERSION"
+
+    curl -L https://github.com/turingwars/turingwars/releases/download/$TURINGWARS_VERSION/turing-wars.tgz > web/turing-wars.tgz
+}
+
+load_from_source() {
+    make -C "$1" build
+    cp "$1/turing-wars.tgz" web/
+}
+
+args=
+source=
+while [ $# -gt 0 ]; do
+    case $1 in
+        "--source")
+            shift
+            source=$1
+        ;;
+        *)
+            args="$args $1"
+        ;;
+    esac
+    shift
+done
+
+if [ ! -z "$source" ]; then
+    load_from_source "$source"
+else
+    load_from_github
+fi
+
+docker-compose up $args
